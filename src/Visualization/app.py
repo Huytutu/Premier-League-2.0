31d207d2ae74dc3bdf0403939e8c3ab99012cc93
Huy Nguyen Gia <huynguyen9605@gmail.com>
2024-12-06 23:38:50 +0700

    Update app.py

--- src/Visualization/app.py
+++ src/Visualization/app.py
@@ -472,14 +472,24 @@ def update_chart(age_bar,age_violin,over_under_25):
     y_club_names = y_club_names[ind]
     fig_over_under_25 = go.Figure()
 
-    # Add bars for 'Under 25'
+    sorted_x_under_25, sorted_y_club_names = zip(*sorted(zip(x_under_25_pro, y_club_names), reverse=False))
+
     fig_over_under_25.add_trace(go.Bar(
-        x=x_under_25_pro,
-        y=y_club_names,
+        x=sorted_x_under_25,
+        y=sorted_y_club_names,
         name='Under 25',
         orientation='h',
         marker=dict(color='#59CAB3', line=dict(color='whitesmoke', width=1.1))
     ))
+
+    # Add bars for 'Under 25'
+    # fig_over_under_25.add_trace(go.Bar(
+    #     x=x_under_25_pro,
+    #     y=y_club_names,
+    #     name='Under 25',
+    #     orientation='h',
+    #     marker=dict(color='#59CAB3', line=dict(color='whitesmoke', width=1.1))
+    # ))
 
     # Add bars for 'Over 25'
     fig_over_under_25.add_trace(go.Bar(
